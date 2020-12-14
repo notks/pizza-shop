4e59caf1b79eb61fc2e5973726cc48ee4dc27fad
notks <kemal.selimbasic.2016@gmail.com>
2020-12-14 02:03:53 +0100

    Updates

---
+++ precache-manifest.fb6c8d0f455d746d2b850e59748ba5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f039cec71730b01278f5dfc6b6261807",
+    "url": "/pizza-shop/index.html"
+  },
+  {
+    "revision": "44ea16a1e4538b69781b",
+    "url": "/pizza-shop/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "91d3304df343e53615e2",
+    "url": "/pizza-shop/static/css/main.5fff5cf0.chunk.css"
+  },
+  {
+    "revision": "44ea16a1e4538b69781b",
+    "url": "/pizza-shop/static/js/2.61fc1575.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pizza-shop/static/js/2.61fc1575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d3304df343e53615e2",
+    "url": "/pizza-shop/static/js/main.7d6e7fbf.chunk.js"
+  },
+  {
+    "revision": "150f900cec618b4fd8cb",
+    "url": "/pizza-shop/static/js/runtime-main.b90cc9f4.js"
+  }
+]);
