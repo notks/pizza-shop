14fb62e1e2c500cc0c3915f403289cff5350d097
notks <kemal.selimbasic.2016@gmail.com>
2021-04-21 15:14:37 +0200

    Updates

---
+++ precache-manifest.1bc43089c66ab818731da9f10d1c76ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23627a80a8b5079eaf29d0e2c5d61485",
+    "url": "/pizza-shop/index.html"
+  },
+  {
+    "revision": "fc6ee722d3df87fd61ae",
+    "url": "/pizza-shop/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "83d893330adad7e56a66",
+    "url": "/pizza-shop/static/css/main.81e96000.chunk.css"
+  },
+  {
+    "revision": "fc6ee722d3df87fd61ae",
+    "url": "/pizza-shop/static/js/2.36927fde.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pizza-shop/static/js/2.36927fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83d893330adad7e56a66",
+    "url": "/pizza-shop/static/js/main.138635f0.chunk.js"
+  },
+  {
+    "revision": "150f900cec618b4fd8cb",
+    "url": "/pizza-shop/static/js/runtime-main.b90cc9f4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pizza-shop/precache-manifest.00527943f09e792a7644ad30891aee72.js"
+  "/pizza-shop/precache-manifest.1bc43089c66ab818731da9f10d1c76ed.js"
 );
 
 self.addEventListener('message', (event) => {
